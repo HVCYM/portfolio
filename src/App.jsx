e3e9822499b424dd62c73651584c1414dd30dfdf
github-himanshu-yadav <himanshuyadav20@hotmail.com>
2023-05-07 01:12:18 +0530

    commented not used components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
 import About from "./components/about/About";
-import Experiences from "./components/experiences/Experiences";
+// import Experiences from "./components/experiences/Experiences";
 import Intro from "./components/intro/Intro";
 import LeftSidebar from "./components/sidebar/LeftSidebar";
 import Projects from "./components/projects/Projects";
-import RightSideBar from "./components/sidebar/RightSidebar";
+// import RightSideBar from "./components/sidebar/RightSidebar";
 
 function App() {
   return (
